28cf639c4684b2cae840cd4cd9b40ac362f85f1d
Lokeshwari K <lokeshwari2209@gmail.com>
2026-01-08 18:54:40 +0530

    Added division operation to calculator

--- calculator.py
+++ calculator.py
@@ -4,3 +4,10 @@ a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 
 print("Addition =", a + b)
+
+
+# DIVISION
+if b != 0:
+    print("Division =", a / b)
+else:
+    print("Division by zero is not allowed")
